b5a4e74e22ffe20f37e581bc75b9b615e7b9abdf
vadychka <dp010694svv@gmail.com>
2018-10-15 10:26:56 +0300

    home_work1

---
+++ home work 1/js/script.js
@@ -0,0 +1,45 @@
+﻿var arr = ['first','second','third','fourth'];
+console.log(arr);
+
+//pop
+arr.pop();
+console.log(arr);
+
+//push
+arr.push('fourth');
+console.log(arr);
+
+//concat
+var arr2 = ['fifth','sixth','seventh'];
+var All_arr = arr.concat(arr2);
+console.log(All_arr);
+
+//indexOf
+var Index = All_arr.indexOf('second');
+console.log(Index);
+var Index = All_arr.indexOf('eighth');
+console.log(Index);
+
+//join
+console.log(All_arr.join('-'));
+
+//forEach
+All_arr.forEach(function (el) {
+    console.log(el);
+})
+
+//slice
+console.log(All_arr.slice(1,6));
+
+//splice
+All_arr.splice(2,2, 'THIRD', 'FOURTH');
+console.log(All_arr);
+
+//shift
+var shiftAll = All_arr.shift();
+console.log(All_arr);
+console.log(shiftAll);
+
+//unshift
+All_arr.unshift('null','first');
+console.log(All_arr);
